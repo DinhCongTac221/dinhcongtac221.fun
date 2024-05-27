da9687dbe2e8fff606c2faeb5c26e0f09e0be172
Dinh Cong Tac <dinhcongtac221@gmail.com>
2024-05-27 22:51:41 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,14 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
-  { name: "Contact", href: "/contact" },
+ 
 ];
 
 const blockchains = [
+  { name: "Polygon", stakeLink: "/polygon/stake", servicesLink: "/polygon/services", logo: "https://static.wixstatic.com/media/7191ae_5988ff0713cc4dac8673d7210f5ca6db~mv2.png/v1/fill/w_216,h_231,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Polygon.png" },
+  { name: "Polkadot", stakeLink: "/polkadot/stake", servicesLink: "/polkadot/services", logo: "https://static.wixstatic.com/media/7191ae_5988ff0713cc4dac8673d7210f5ca6db~mv2.png/v1/fill/w_216,h_231,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Polygon.png" },
+  { name: "Kusama", stakeLink: "/kusama/stake", servicesLink: "/kusama/services", logo: "https://static.wixstatic.com/media/7191ae_5988ff0713cc4dac8673d7210f5ca6db~mv2.png/v1/fill/w_216,h_231,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Polygon.png" },
+  { name: "Near", stakeLink: "/near/stake", servicesLink: "/near/services", logo: "https://static.wixstatic.com/media/7191ae_5988ff0713cc4dac8673d7210f5ca6db~mv2.png/v1/fill/w_216,h_231,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Polygon.png" },
   { name: "Polygon", stakeLink: "/polygon/stake", servicesLink: "/polygon/services", logo: "https://static.wixstatic.com/media/7191ae_5988ff0713cc4dac8673d7210f5ca6db~mv2.png/v1/fill/w_216,h_231,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Polygon.png" },
   { name: "Polkadot", stakeLink: "/polkadot/stake", servicesLink: "/polkadot/services", logo: "https://static.wixstatic.com/media/7191ae_5988ff0713cc4dac8673d7210f5ca6db~mv2.png/v1/fill/w_216,h_231,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Polygon.png" },
   { name: "Kusama", stakeLink: "/kusama/stake", servicesLink: "/kusama/services", logo: "https://static.wixstatic.com/media/7191ae_5988ff0713cc4dac8673d7210f5ca6db~mv2.png/v1/fill/w_216,h_231,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Polygon.png" },
@@ -37,20 +41,20 @@ export default function Home() {
       />
       <div className="flex flex-col items-center justify-center flex-grow">
         <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-          chronark
+          DinhcongTac221
         </h1>
       </div>
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I'm DinhCongTac221{" "}
+          I'm {" "}
           <Link
             target="_blank"
-            href="https://dinhcongtac221.fun"
+            href="https://github.com/dinhcongtac221"
             className="underline duration-500 hover:text-zinc-300"
-          >
-            a blockchain enthusiast
-          </Link>, I have been involved in crypto for 7 years. I am passionate about contributing to projects and running nodes..
+          >DinhCongTac221
+            
+          </Link>a blockchain enthusiast, I have been involved in crypto for 7 years. I am passionate about contributing to projects and running nodes..
         </h2>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-16">
