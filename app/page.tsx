2e3ed529ed42b55363a068c6229a0259b5968d38
Dinh Cong Tac <dinhcongtac221@gmail.com>
2024-05-27 22:59:23 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import Link from "next/link";
 import React from "react";
-import Particles from "./components/particles";
-
-
-
 const blockchains = [
   { name: "Polygon", stakeLink: "/polygon/stake", servicesLink: "/polygon/services", logo: "https://static.wixstatic.com/media/7191ae_5988ff0713cc4dac8673d7210f5ca6db~mv2.png/v1/fill/w_216,h_231,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Polygon.png" },
   { name: "Polkadot", stakeLink: "/polkadot/stake", servicesLink: "/polkadot/services", logo: "https://static.wixstatic.com/media/7191ae_5988ff0713cc4dac8673d7210f5ca6db~mv2.png/v1/fill/w_216,h_231,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Polygon.png" },
@@ -25,10 +21,7 @@ export default function Home() {
         </ul>
       </nav>
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <Particles
-        className="absolute inset-0 -z-10 animate-fade-in"
-        quantity={100}
-      />
+   
       <div className="flex flex-col items-center justify-center flex-grow">
         <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
           DinhcongTac221
